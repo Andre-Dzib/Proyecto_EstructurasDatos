099b297b32eedfc74b6c0df3c28a0dac4a954637
Alexis-Morales-Solis <166084327+Alexis-Morales@users.noreply.github.com>
2026-02-08 13:37:51 -0600

    Update Parada.java
    coments

--- src/main/java/com/gestion_portuaria/Distribucion/Parada.java
+++ src/main/java/com/gestion_portuaria/Distribucion/Parada.java
@@ -1,32 +1,38 @@
 package com.gestion_portuaria.Distribucion;
 
 public class Parada {
-    private int id;
-    private String nombre;
-
+    private int id;        
+    private String nombre; 
+    
+    // Constructor: Crea una nueva parada con ID y nombre
     public Parada(int id, String nombre) {
-        this.id = id;
-        this.nombre = nombre;
+        this.id = id;        
+        this.nombre = nombre; 
     }
 
+    
     public int getId() {
-        return id;
+        return id;  // Retorna el valor del atributo id
     }
 
+   
     public void setId(int id) {
-        this.id = id;
+        this.id = id;  // Asigna el valor id
     }
 
+   
     public String getNombre() {
-        return nombre;
+        return nombre;  // Retorna el valor del atributo nombre
     }
 
+   
     public void setNombre(String nombre) {
-        this.nombre = nombre;
+        this.nombre = nombre;  // Asigna el atributo nombre
     }
 
+    // Sobrescribe el método retorna representación en texto de la parada
     @Override
     public String toString() {
-        return nombre + " (" + id + ")";
+        return nombre + " (" + id + ")";  
     }
 }
